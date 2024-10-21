17e7d56640442e02fac3f9b6f65089eef64ff9e9
Duchemil <Lucas-76700@hotmail.fr>
2024-10-21 14:25:09 +0200

    first commit

---
+++ Transcendence/Frontend/src/pages/Matchmaking.jsx
@@ -0,0 +1,14 @@
+// src/pages/Matchmaking.jsx
+import React from 'react';
+import "../App.css";
+
+const Matchmaking = () => {
+  return (
+    <div>
+      <h1>Matchmaking in Progress...</h1>
+      <p>We're finding the best match for you!</p>
+    </div>
+  );
+};
+
+export default Matchmaking;

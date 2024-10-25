06cbfe6d7e77a7f5cc24f407677ddd5fced13545
ldaniel <habbodu76@gmail.com>
2024-10-25 14:16:12 +0200

    Static files + admin working again
    Changed dockerfiles / nginx https.conf for the staticfiles to be recognized, + added a volume to the docker-compose so that nginx could access static

--- Transcendence/Backend/Transcendence/urls.py
+++ Transcendence/Backend/Transcendence/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  # Add this import
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pong.urls')), #include ulrs from pong app
+    path('', include('pong.urls')),  # Include URLs from pong app
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

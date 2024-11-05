df5e81ed8f887fc391bc33bb26ae7dd6501a2dc5
Duchemil <Lucas-76700@hotmail.fr>
2024-11-05 16:12:57 +0100

    gitignore + login redirect

--- Transcendence/Frontend/src/LoginPage.jsx
+++ Transcendence/Frontend/src/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
 const LoginPage = () => {
@@ -6,6 +6,14 @@ const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('authToken');
+    if (authToken) {
+      // Redirect to the main page if already logged in
+      navigate('/menu');
+    }
+  }, [navigate]);
 
   const handleLogin = async () => {
     try {
@@ -16,9 +24,9 @@ const LoginPage = () => {
         },
         body: JSON.stringify({ username, password }),
       });
-  
+
       const data = await response.json();
-  
+
       if (response.ok) {
         localStorage.setItem('authToken', data.token);
         console.log('Login successful!');
@@ -58,7 +66,7 @@ const LoginPage = () => {
         onKeyDown={handleKeyDown} // Add onKeyDown to trigger Enter
       />
       <button onClick={handleLogin}>Login</button>
-      
+
       {/* Link to the registration page */}
       <p>
         Don't have an account? <Link to="/register">Register here</Link>
